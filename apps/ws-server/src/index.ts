31801a59da6f55caa8b4d71ecb0ce049add50d05
ChetanBhosale <chetanbhosale810@gmail.com>
2025-03-01 19:54:41 +0530

    fix

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -4,22 +4,30 @@ import { JWT_SECRET } from "@repo/ts-common/secret";
 
 const wss = new WebSocketServer({ port: 8080 });
 console.log("application started successfully");
-wss.on("connection", (ws) => {
-  console.log("Client connected");
+wss.on("connection", (ws,request) => {
+  const url = request.url;
+  if(!url) {
+    ws.close();
+    return;
+  }
+  const token = url.split("/")[1];
+  if(!token) {
+    ws.close();
+    return;
+  }
 
-  ws.on("message", (message:any) => {
+  const decoded = jwt.verify(token,JWT_SECRET as string)
+  if(!decoded) {
+    ws.close();
+    return;
+  } 
 
+  ws.on("message",(message) => {
     const parsedMessage = JSON.parse(message.toString());
-    if(parsedMessage.token) {
-      const decoded = jwt.verify(parsedMessage.token,JWT_SECRET as string)
-      console.log({decoded})
-      if(!decoded) {
-        return ws.send(JSON.stringify({message : "Unauthorized"}))
-      }
-    }else{
-      return ws.send(JSON.stringify({message : "Unauthorized"}))
+    if(parsedMessage.type === "ping") {
+      ws.send(JSON.stringify({type : "pong"}));
     }
-  });
+  })
   
   
 });
